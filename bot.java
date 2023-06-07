a5d6e26c18e814e61e1587ddbfec03001778aced
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-06-07 19:34:14 +0500

    Update bot.java

--- bot.java
+++ bot.java
@@ -12,7 +12,7 @@ public class ChatBot {
             } else if (input.contains("news")) {
                 System.out.println("The latest news is that there is a new virus outbreak in Asia.");
             } else if (input.contains("movie")) {
-                System.out.println("The best movie to watch right now is The Godfather.");
+                System.out.println("The best movie to watch right now is Tom and Jerry.");
             } else if (input.contains("restaurant")) {
                 System.out.println("The best restaurant in town is Chez Jacques.");
             } else if (input.contains("exit")) {
